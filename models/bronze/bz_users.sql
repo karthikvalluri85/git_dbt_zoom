54382221379bbf8bce77992f20558df77765bc3a
Karthik Valluri <karthikvalluri@gmail.com>
2025-10-11 17:32:34 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_users.sql
+++ models/bronze/bz_users.sql
@@ -1,29 +1,6 @@
 {{{
   config(
-    materialized='table',
-    pre_hook=[
-      "{% if execute and this.identifier != 'bz_audit_log' %}
-        {% set start_time = modules.datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') %}
-        INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-        SELECT
-          'users',
-          CURRENT_TIMESTAMP(),
-          CURRENT_USER(),
-          0,
-          'PROCESSING'
-      {% endif %}"
-    ],
-    post_hook=[
-      "{% if execute and this.identifier != 'bz_audit_log' %}
-        INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
-        SELECT
-          'users',
-          CURRENT_TIMESTAMP(),
-          CURRENT_USER(),
-          DATEDIFF('SECOND', '{{ run_started_at }}', CURRENT_TIMESTAMP()),
-          'SUCCESS'
-      {% endif %}"
-    ]
+    materialized='table'
   )
 }}}
 
