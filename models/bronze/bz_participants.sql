09b1401faf08cb0b643aab0a07054bd564e9057a
Karthik Valluri <karthikvalluri@gmail.com>
2025-10-11 17:30:59 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_participants.sql
+++ models/bronze/bz_participants.sql
@@ -1,22 +1,40 @@
-/*
-* Model: bz_participants
-* Description: Bronze layer table for participant data from Zoom
-*/
-
-{{ config(
+{{{
+  config(
     materialized='table',
-    unique_key='participant_id'
-) }}
+    pre_hook=[
+      "{% if execute and this.identifier != 'bz_audit_log' %}
+        {% set start_time = modules.datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') %}
+        INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+        SELECT
+          'participants',
+          CURRENT_TIMESTAMP(),
+          CURRENT_USER(),
+          0,
+          'PROCESSING'
+      {% endif %}"
+    ],
+    post_hook=[
+      "{% if execute and this.identifier != 'bz_audit_log' %}
+        INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+        SELECT
+          'participants',
+          CURRENT_TIMESTAMP(),
+          CURRENT_USER(),
+          DATEDIFF('SECOND', '{{ run_started_at }}', CURRENT_TIMESTAMP()),
+          'SUCCESS'
+      {% endif %}"
+    ]
+  )
+}}}
 
--- Extract and transform data from raw participants table
+-- Transform raw participants data to bronze layer
 SELECT
-    participant_id,
-    meeting_id,
-    user_id,
-    join_time,
-    leave_time,
-    -- Metadata columns
-    CURRENT_TIMESTAMP() as load_timestamp,
-    CURRENT_TIMESTAMP() as update_timestamp,
-    'ZOOM_PLATFORM' as source_system
-FROM ZOOM_DATABASE.RAW.participants
+  participant_id,
+  meeting_id,
+  user_id,
+  join_time,
+  leave_time,
+  CURRENT_TIMESTAMP() AS load_timestamp,
+  CURRENT_TIMESTAMP() AS update_timestamp,
+  'ZOOM_PLATFORM' AS source_system
+FROM {{ source('raw', 'participants') }}
