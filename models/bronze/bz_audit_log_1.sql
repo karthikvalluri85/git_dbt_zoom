3c295fbaf894a1b626dbf399a83ee68f7e541588
Karthik Valluri <karthikvalluri@gmail.com>
2025-10-11 17:34:28 +0530

    Automated DBT Sync from AAVA

---
+++ models/bronze/bz_audit_log_1.sql
@@ -0,0 +1,9 @@
+-- Create audit log table to track processing of bronze models
+SELECT
+  1 AS record_id,
+  'test' AS source_table,
+  CURRENT_TIMESTAMP() AS load_timestamp,
+  CURRENT_USER() AS processed_by,
+  0 AS processing_time,
+  'INITIALIZED' AS status
+LIMIT 0
