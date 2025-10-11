901fad2f53afa938d6e1768971e006a57082795f
Karthik Valluri <karthikvalluri@gmail.com>
2025-10-11 17:39:32 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_licenses.sql
+++ models/bronze/bz_licenses.sql
@@ -1,6 +1,14 @@
 {{{
   config(
-    materialized='table'
+    materialized='table',
+    pre_hook=[
+      "INSERT INTO {{ this.schema }}.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status) 
+       SELECT 'licenses', CURRENT_TIMESTAMP(), CURRENT_USER(), 0, 'PROCESSING'"
+    ],
+    post_hook=[
+      "INSERT INTO {{ this.schema }}.bz_audit_log (source_table, load_timestamp, processed_by, processing_time, status) 
+       SELECT 'licenses', CURRENT_TIMESTAMP(), CURRENT_USER(), DATEDIFF('SECOND', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()), 'SUCCESS'"
+    ]
   )
 }}}
 
@@ -11,7 +19,7 @@ SELECT
   assigned_to_user_id,
   start_date,
   end_date,
-  load_timestamp,
-  update_timestamp,
-  source_system
+  CURRENT_TIMESTAMP() AS load_timestamp,
+  CURRENT_TIMESTAMP() AS update_timestamp,
+  'ZOOM_PLATFORM' AS source_system
 FROM RAW.licenses
