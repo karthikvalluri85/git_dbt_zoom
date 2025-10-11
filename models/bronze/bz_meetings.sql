3eb093120bf37626e31ed6321eaca4c5f1251bb4
Karthik Valluri <karthikvalluri@gmail.com>
2025-10-11 17:30:58 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_meetings.sql
+++ models/bronze/bz_meetings.sql
@@ -1,23 +1,41 @@
-/*
-* Model: bz_meetings
-* Description: Bronze layer table for meeting data from Zoom
-*/
-
-{{ config(
+{{{
+  config(
     materialized='table',
-    unique_key='meeting_id'
-) }}
+    pre_hook=[
+      "{% if execute and this.identifier != 'bz_audit_log' %}
+        {% set start_time = modules.datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') %}
+        INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+        SELECT
+          'meetings',
+          CURRENT_TIMESTAMP(),
+          CURRENT_USER(),
+          0,
+          'PROCESSING'
+      {% endif %}"
+    ],
+    post_hook=[
+      "{% if execute and this.identifier != 'bz_audit_log' %}
+        INSERT INTO {{ ref('bz_audit_log') }} (source_table, load_timestamp, processed_by, processing_time, status)
+        SELECT
+          'meetings',
+          CURRENT_TIMESTAMP(),
+          CURRENT_USER(),
+          DATEDIFF('SECOND', '{{ run_started_at }}', CURRENT_TIMESTAMP()),
+          'SUCCESS'
+      {% endif %}"
+    ]
+  )
+}}}
 
--- Extract and transform data from raw meetings table
+-- Transform raw meetings data to bronze layer
 SELECT
-    meeting_id,
-    host_id,
-    meeting_topic,
-    start_time,
-    end_time,
-    duration_minutes,
-    -- Metadata columns
-    CURRENT_TIMESTAMP() as load_timestamp,
-    CURRENT_TIMESTAMP() as update_timestamp,
-    'ZOOM_PLATFORM' as source_system
+  meeting_id,
+  host_id,
+  meeting_topic,
+  start_time,
+  end_time,
+  duration_minutes,
+  CURRENT_TIMESTAMP() AS load_timestamp,
+  CURRENT_TIMESTAMP() AS update_timestamp,
+  'ZOOM_PLATFORM' AS source_system
 FROM {{ source('raw', 'meetings') }}
