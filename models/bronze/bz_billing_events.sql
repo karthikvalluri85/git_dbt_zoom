e8eb559e4a525e94520832de06c4559e9dfd2023
Karthik Valluri <karthikvalluri@gmail.com>
2025-10-11 17:37:33 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_billing_events.sql
+++ models/bronze/bz_billing_events.sql
@@ -11,7 +11,7 @@ SELECT
   event_type,
   amount,
   event_date,
-  CURRENT_TIMESTAMP() AS load_timestamp,
-  CURRENT_TIMESTAMP() AS update_timestamp,
-  'ZOOM_PLATFORM' AS source_system
-FROM {{ source('raw', 'billing_events') }}
+  load_timestamp,
+  update_timestamp,
+  source_system
+FROM RAW.billing_events
