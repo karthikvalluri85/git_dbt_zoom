2e45ea1cf8fc84860f5c00829fc4bb764133f717
Karthik Valluri <karthikvalluri@gmail.com>
2025-10-11 17:37:30 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_users.sql
+++ models/bronze/bz_users.sql
@@ -1,10 +1,17 @@
+{{{
+  config(
+    materialized='table'
+  )
+}}}
+
+-- Transform raw users data to bronze layer
 SELECT
   user_id,
   user_name,
   email,
   company,
   plan_type,
-  current_timestamp() as load_timestamp,
-  current_timestamp() as update_timestamp,
-  'ZOOM_PLATFORM' as source_system
-FROM {{ source('raw', 'users') }}
+  load_timestamp,
+  update_timestamp,
+  source_system
+FROM RAW.users
