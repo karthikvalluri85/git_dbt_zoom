4eb52bab1ceb16b0a9fe07f13a1956004a2d7ab5
Karthik Valluri <karthikvalluri@gmail.com>
2025-10-11 17:32:36 +0530

    Automated DBT Sync from AAVA

--- models/bronze/bz_webinars.sql
+++ models/bronze/bz_webinars.sql
@@ -1,23 +1,18 @@
-/*
-* Model: bz_webinars
-* Description: Bronze layer table for webinar data from Zoom
-*/
+{{{
+  config(
+    materialized='table'
+  )
+}}}
 
-{{ config(
-    materialized='table',
-    unique_key='webinar_id'
-) }}
-
--- Extract and transform data from raw webinars table
+-- Transform raw webinars data to bronze layer
 SELECT
-    webinar_id,
-    host_id,
-    webinar_topic,
-    start_time,
-    end_time,
-    registrants,
-    -- Metadata columns
-    CURRENT_TIMESTAMP() as load_timestamp,
-    CURRENT_TIMESTAMP() as update_timestamp,
-    'ZOOM_PLATFORM' as source_system
-FROM ZOOM_DATABASE.RAW.webinars
+  webinar_id,
+  host_id,
+  webinar_topic,
+  start_time,
+  end_time,
+  registrants,
+  CURRENT_TIMESTAMP() AS load_timestamp,
+  CURRENT_TIMESTAMP() AS update_timestamp,
+  'ZOOM_PLATFORM' AS source_system
+FROM {{ source('raw', 'webinars') }}
